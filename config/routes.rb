5ebe1b7d03a61ecefc410383fc2ed595babedd7d
紀　祐司 <signtheswish@gmail.com>
2018-10-17 15:35:39 +0900

    add_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:index :edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :show, :edit, :update] do
-    resources :messages, [:index, :create]
+    resources :messages, only: [:index, :create]
   end
 end
