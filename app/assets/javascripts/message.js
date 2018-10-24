c60d158505494bba63a0a368cfb0e1333fa34b61
紀　祐司 <signtheswish@gmail.com>
2018-10-24 12:07:45 +0900

    add-asynchronous

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,51 @@
+$(function(){
+    function buildHTML(message){
+      var insertImage = '';
+      if (message.image){
+        insertImage = `<img src="${message.image}">`;
+      }
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="message-name">
+                        ${ message.name}
+                      </div>
+                      <div class="message-time">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-massage">
+                      <div class="message-body">
+                        <p class="message-body__content">
+                        ${message.content}
+                        </p>
+                      </div>
+                        ${insertImage}
+                    </div>
+                </div>`
+      return html;
+    }
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+  .done(function(message){
+    console.log(message);
+    var html = buildHTML(message);
+    $('.messages').append(html)
+    $('#new_message')[0].reset();
+    $('.messages.js-message').animate({scrollTop: $('.messages.js-message')[0].scrollHeight}, 'fast');
+  })
+  .fail(function(){
+    alert('error');
+    })
+  return false;
+  });
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,13 +4,17 @@ class MessagesController < ApplicationController
 
 	def index
     @message = Message.new
-    @messages = @group.messages.includes(:user)
-	end
+    @messages = @group.messages.includes(:user).order("created_at ASC")
+  end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: "メッセージを投稿しました"
+      respond_to do |format|
+        format.html {redirect_to group_messages_path(@group)}
+        format.json
+      end
+       flash[:notice] =  "メッセージを投稿しました"
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = "メッセージを入力してください。"
